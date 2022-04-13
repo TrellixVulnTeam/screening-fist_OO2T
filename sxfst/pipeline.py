5b0b8b020036511f84a9f91f58fd0103d2547e06
jamesengleback <jamesengleback@hotmail.co.uk>
2022-04-13 16:33:24 +0000

    compound lookup seems to work, working on signal processing, todo: plotting, anomaly detection, michaelis menten

--- sxfst/pipeline.py
+++ sxfst/pipeline.py
@@ -8,6 +8,7 @@ import ast
 from functools import lru_cache
 
 import pandas as pd
+from scipy.ndimage import gaussian_filter1d
 
 from sxfst import utils
 
@@ -63,6 +64,14 @@ class PlatereaderFiles:
                  ):
         self.find_results = find_results
 
+class Cpd:
+    def __init__(self,
+                 **kwargs,
+                 ):
+        self.__dict__ = {**self.__dict__, **kwargs}
+    def __repr__(self):
+        return f"Cpd: {pformat(self.__dict__)}"
+
 class Screen:
     ''' Container for screening data.
         Maps compounds to plate wells.
@@ -75,6 +84,17 @@ class Screen:
                  ):
         self.map_fs(path)
         self.map_wells()
+    def __getitem__(self, idx):
+        assert hasattr(self, 'cpd_map')
+        assert idx in self.cpd_map.keys()
+        return self.cpd_map[idx]
+    def __iter__(self):
+        assert hasattr(self, 'cpd_map')
+        for i in self.cpd_map:
+            yield self[i]
+    def __len__(self):
+        assert hasattr(self, 'cpd_map')
+        return len(self.cpd_map)
     def __repr__(self):
         return f"{self.fs}"
     def map_fs(self, path):
@@ -89,6 +109,7 @@ class Screen:
         '''
         fs = find(path)
         self.fs = fs
+        self.root = path
         self.echo_files = EchoFiles(fs)
         self.platereader_files = PlatereaderFiles(fs)
         self.config = Config(fs)
@@ -116,21 +137,20 @@ class Screen:
                 assert len(echo_name_) == 1
                 echo_name = echo_name_[0]
                 wells = list(chunk['DestWell'])
-                m_ = map_echo_name_2path(echo_name, self.config)
-                if m_ is not None:
-                    m = m_[list(m_.keys())[0]]
-                    if 'test' in m:
-                        o['test'] = read_plate_csv(m['test'][0])[wells]
-                    if 'control' in m:
-                        o['ctrl'] = m['control']
-                    if 'ctrl' in m:
-                        o['ctrl'] = m['ctrl']
-                    if 'echo_map' in m or 'echo_map' in m:
-                        o['echo_map'] = m['echo_map']
-                cpd_map[cpd] = o
-            pprint(cpd_map)
-        else:
-            pass #print(self.picklists)
+                m = map_echo_name_2path(echo_name, 
+                                        self.config,
+                                        self.root,
+                                       )
+                if m is not None:
+                   ctrl = read_plate_csv(m['ctrl'])[wells] if 'ctrl' in m.keys() else None
+                   test = read_plate_csv(m['test'])[wells] if 'test' in m.keys() else None
+                   echo_map = m['echo_map'] if 'echo_map' in m.keys() else None
+                   cpd_map[cpd] = Cpd(name=cpd,
+                                      test=test,
+                                      ctrl=ctrl,
+                                      echo_map=chunk,
+                                      )
+            self.cpd_map = cpd_map
 
 @lru_cache(128)
 def read_plate_csv(path):
@@ -147,23 +167,88 @@ def proc_picklist(path):
     return picklist
 
 @lru_cache(128)
-def map_echo_name_2path(echo_name, config):
+def map_echo_name_2path(echo_name, config, root):
+    add_root = lambda path : os.path.join(root, path)
     platereader_files = config['platereader']
+    o = {}
     for i,j in zip(platereader_files.keys(),
                    platereader_files.values()):
         item = j['echo_map']
         if item is not None:
             if echo_name == item[0]:
-                return {i:j}
+                o['test'] = add_root(j['test'][0])
+                o['control'] = add_root(j['control'][0])
+                o['echo_map'] = add_root(j['echo_map'][0])
+                return o
 
-def proc(path):
-    screen = Screen(path)
-    #print(screen.config)
-    #pprint(screen)
+def norm_traces(test, 
+                ctrl=None):
+    '''
+    '''
+    test = test.subtract(test.loc[:,800],
+                         axis=0)
+    if ctrl is not None:
+        ctrl = ctrl.subtract(ctrl.loc[:,800],
+                             axis=0)
+        norm = test - ctrl
+        return norm
+    else:
+        return test
+
+def smooth(df,
+           sigma=3,
+           ):
+    cols = df.columns
+    idx = df.index
+    smth = pd.DataFrame(gaussian_filter1d(df, sigma),
+                        index=idx,
+                        columns=cols)
+    return df
+
+def is_anomaly(norm):
+    return False
+
+def diff(norm, baseline):
+    return norm.subtract(baseline,
+                         axis=0)
+
+def response(_diff):
+    a420 = _diff.loc[:,420]
+    a390 = _diff.loc[:,390]
+    return a420.abs().add(a390.abs())
+
+def c2(v1, c1, v2):
+    return (v1 * c1) / v2
+
+def proc(cpd, # Cpd()
+         sigma=3,
+         ):
+    name = cpd.name
+    test = cpd.test
+    ctrl = cpd.ctrl
+    echo_map = cpd.echo_map
+    norm = norm_traces(test, ctrl)
+    smth = smooth(norm, sigma)
+    if not is_anomaly(smth):
+        change = diff(smth, smth[list(smth.keys())[0]])
+        y = dict(response(change))
+        #print(y)
+        vol_well_map = dict(zip(echo_map['DestWell'],
+                                echo_map['Transfer Volume /nl']))
+        x = {i:c2(v1=vol_well_map[i] * 1e-9,    # nl to l
+                  c1=10e-3,                     # 10 mM stock
+                  v2=40e-6,                     # 40 ul well vol
+                  ) / 1e-6                     # M to uM
+              for i in vol_well_map}
+        print(x)
+        print(y)
+
 
 def main(args):
-    for i in args:
-        proc(i)
+    for path in args:
+        screen = Screen(path)
+        for cpd in screen:
+            proc(cpd)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
