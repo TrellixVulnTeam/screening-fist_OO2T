379629355daa3385b88f69f6303b098dc80bd1c4
james <james@pop-os.localdomain>
2022-05-31 16:10:09 +0100

    ubuntu 20.04 ok, working on cuda install stuff

--- model/setup-gpu.sh
+++ model/setup-gpu.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
+mkdir src
+cd src/
 
 sudo apt update && sudo apt upgrade -y && sudo apt install git neovim -y
 
 # cuda
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
 
-mkdir src
-cd src/
 
 git clone https://github.com/jamesengleback/dotfiles
 cd dotfiles
 ./install-debian.sh
 ./setup-config.sh
 ./install-miniconda.sh
+source ~/.bashrc
+
+echo 'PS1="\[\033[01;93m\] [gpu] \w \[\033[01;93m\]\$ \[\033[00m\]"' >> ~/.bashrc
+cd ~/
+git clone https://github.com/jamesengleback/screening-fist
+cd screening-fist/model
+conda env create -f rdk-tch
