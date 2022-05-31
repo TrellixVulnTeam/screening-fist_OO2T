5152c3a6862ee1cdfb128dadd9f284e2029b8aed
james <james@pop-os.localdomain>
2022-05-31 14:28:29 +0100

    debian11 no start, debian10 no drivers at https://developer.nvidia.com/cuda-downloads, tring ubuntu 22.04

--- model/mkinstance.sh
+++ model/mkinstance.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-linode-cli linodes create --type 'g1-gpu-rtx6000-1'  --root_pass $1  --region 'us-east'  --label 'gpu'  --image 'linode/debian10'  --authorized_keys 'ssh-rsa'
+linode-cli linodes create --type 'g1-gpu-rtx6000-1'  --root_pass $1  --region 'us-east'  --label 'gpu'  --image 'linode/ubuntu22.04'  --authorized_keys 'ssh-rsa'
--- model/setup-gpu.sh
+++ model/setup-gpu.sh
@@ -3,11 +3,11 @@
 sudo apt update && sudo apt upgrade -y && sudo apt install git neovim -y
 
 # cuda
-sudo apt install build-essential linux-headers-$(uname -r)
-wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-debian11-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo dpkg -i cuda-repo-debian11-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo cp /var/cuda-repo-debian11-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo add-apt-repository contrib
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
 
